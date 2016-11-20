6c7d7adefba2321d5d81d4eb8a6d727450b2cd60
AdrianMoPe <adrianmoraperela@gmail.com>
2016-11-20 19:00:00 +0100

    Creacion del proyecto

---
+++ DGT/src/main/java/es/uclm/es/isoft2/SwBuilders/DGT/App.java
@@ -0,0 +1,13 @@
+package es.uclm.es.isoft2.SwBuilders.DGT;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
