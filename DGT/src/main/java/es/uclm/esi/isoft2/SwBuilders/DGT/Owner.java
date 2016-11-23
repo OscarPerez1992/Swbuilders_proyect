8e29df77f02b179830562a028fe6f01a5c1e4d14
Oscar <Oscar@172.19.152.140>
2016-11-23 22:57:43 +0100

    commit Añadidas clases correspondientes IT1 a src junto con el diagrama de clases de diseño

---
+++ DGT/src/main/java/es/uclm/esi/isoft2/SwBuilders/DGT/Owner.java
@@ -0,0 +1,20 @@
+public class Owner {
+	private Object _inquiries;
+	private Object _vehicles;
+
+	public void getInquiries()  {
+		return this._inquiries;
+	}
+
+	public void setInquiries(Object aInquiries) {
+		this._inquiries = aInquiries;
+	}
+
+	public void getVehicles() {
+		return this._vehicles;
+	}
+
+	public void setVehicles(Object aVehicles) {
+		this._vehicles = aVehicles;
+	}
+}
