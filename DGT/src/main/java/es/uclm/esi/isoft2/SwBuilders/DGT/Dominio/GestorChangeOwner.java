de69247bbc9eac7d2760c2d134c4bb5218881f60
Oscar <Oscar@DESKTOP-BEUJC36>
2016-11-24 20:52:39 +0100

    Commit actualizacion de diseño iteracciones 1,2 y 3 ,establecimiento de dependencias entre artefactos

---
+++ DGT/src/main/java/es/uclm/esi/isoft2/SwBuilders/DGT/Dominio/GestorChangeOwner.java
@@ -0,0 +1,20 @@
+public class GestorChangeOwner {
+	private Object license;
+	private Object newDni;
+
+	public void getLicense() {
+		return this.license;
+	}
+
+	public void setLicense(Object aLicense) {
+		this._license = aLicense;
+	}
+
+	public void getNewDni() {
+		return this._newDni;
+	}
+
+	public void setNewDni(Object aNewDni) {
+		this._newDni = aNewDni;
+	}
+}
