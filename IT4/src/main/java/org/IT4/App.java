a5851dfaaa7d99a7d4ad3546fa0d48b1f904087c
Christian <Christian@Christian-PC>
2016-11-28 22:07:06 +0100

    Commit de la implementación de la iteración 4

--- IT4/src/main/java/org/IT4/App.java
+++ IT4/src/main/java/org/IT4/App.java
@@ -3,11 +3,13 @@ package org.IT4;
 /**
  * Hello world!
  *
+ *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+	
+    public static void main( String[] args ){
+    	
+        System.out.println( "Hola Mundo" );
+        
     }
 }
