b6fb613aca4aa10edaf971ad9aff93adb52554ad
Oscar <Oscar@DESKTOP-BEUJC36>
2016-12-21 18:56:51 +0100

    commit prueba branch

--- SwBuilders/IT4/src/test/java/org/IT4/testPrueba.java
+++ SwBuilders/IT4/src/test/java/org/IT4/testPrueba.java
@@ -16,7 +16,7 @@ public class testPrueba extends TestCase {
  */
  //
 	@Test
-	public void test31_30() {
+	public void test31_30()  {
 		GeneralDao <Inquiry>gdao=new GeneralDao<>();
 
 		Manager m = Manager.get();
